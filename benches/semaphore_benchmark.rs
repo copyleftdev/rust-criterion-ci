4084ef91f02076cf366e58eddb0a39a965442cbb
Don Johnson <dj@codetestcode.io>
2024-07-24 16:57:52 -0700

    updated version of baseline

--- benches/semaphore_benchmark.rs
+++ benches/semaphore_benchmark.rs
@@ -1,6 +1,8 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion, BenchmarkId};
 use criterion::async_executor::FuturesExecutor;
 use rust_semaphore::MySemaphore;
+use std::time::Duration;
+use tokio::runtime::Runtime;
 
 async fn acquire_semaphore(semaphore: &MySemaphore) {
     let _permit = semaphore.acquire().await.unwrap();
@@ -9,9 +11,18 @@ async fn acquire_semaphore(semaphore: &MySemaphore) {
 fn semaphore_benchmark(c: &mut Criterion) {
     let semaphore = MySemaphore::new(10);
 
-    c.bench_function("semaphore acquire", |b| {
+    let mut group = c.benchmark_group("semaphore_benchmark");
+    group
+        .measurement_time(Duration::new(5, 0))
+        .sample_size(10)
+        .warm_up_time(Duration::new(3, 0))
+        .save_baseline("new"); // Save the baseline
+
+    group.bench_function(BenchmarkId::new("semaphore acquire", 10), |b| {
         b.to_async(FuturesExecutor).iter(|| acquire_semaphore(&semaphore));
     });
+
+    group.finish();
 }
 
 criterion_group!(benches, semaphore_benchmark);
